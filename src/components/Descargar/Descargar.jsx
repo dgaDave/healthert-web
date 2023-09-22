d7e47f21a3df235482d0370203e40a0a16b5eb5f
Jairo Gonzalez <jairoirineo16@outlook.com>
2023-09-22 00:43:19 -0600

    Reorganizacion de la estructura, falta arreglar subtitulo y boton responsive (agregar carrusel)

--- src/components/Descargar/Descargar.jsx
+++ src/components/Descargar/Descargar.jsx
@@ -1,40 +1,34 @@
 import Button from '../Button/Button';
 import Logo from './img/Logo1.svg'
+import './Descargar.css'
 const Descargar = () => {
     return (
-        <div className="contetendorPadre">
-            <div className="contenedorSuperior">
-                <div className="contenedorSuperior--izq">
-                    <div className="contenedorSuperior--izq__img">
-                            <img className='h-20 w-20'
-                                src={Logo}
-                                alt='Logo Healthert'
-                            />
-                    </div>
-                    <div className='contenedorSuperior--izq__text'>
-                        <h1>
-                            HEALTHERT
-                        </h1>
-                        <p>Aplicación móvil de monitoreo y alerta remota</p>
-                        <Button text={"Descargar aquí"} link={"#"}></Button>
-                    </div>
+        
+        <div>
+            <div className='flex grid-cols-2 gap-2'>
+                <div className='flex justify-center ml-10  mt-8 h-24 w-24 min-w-max min-h-max md:h-32 md:w-32 lg:h-40 lg:w-40 transform duration-700'>
+                    <img src={Logo} />
                 </div>
-                <div className="contenedorSuperior--der">
-
+                <div className='grip grip-cols-2   justify-center mr-1 ml-6 mt-8 h-24 w-24 md:ml-10 md:mt-10 lg:ml-20'>
+                    <div className='flex text-black text-3xl font-bold tracking-wider md:text-4xl lg:text-5xl transform duration-700'>
+                        <h1>Healthert</h1>
+                    </div>
+                    <div className='flex justify-center text-black text-sm '>
+                        <p>Aplicación móvil de monitoreo y alerta remota</p>
+                    </div>
+                    <div className='text-sm'>
+                        <Button link={""} text={"Descargar"}></Button>
+                    </div>
                 </div>
             </div>
-            <div className="contenedorBajo">
-                <div className="contenedorBajo--izq">
-
+            <div className='flex grid-cols-1 gap-2 lg:grid-cols-2'>
+                <div>
+                   {/*Div para carrusel*/} 
                 </div>
-                <div className="contenedorBajo--der">
-                    <p>
-                    Healthert permite al usuario realizar un seguimiento de los datos médicos de algún familiar o paciente en tiempo real.<br/>
-                    Solo descarga la aplicación en tu telefono móvil y conecta con un reloj inteligente para tener un monitoreo del ritmo cardiaco, ubicación y recordatorio de medicamentos.<br/>
-                    Esta aplicación te ayudará a mantener un buen seguimiento del estado de salud de tu familiar o paciente que porta el reloj, de esta manera, al registrarse un dato anormal en las mediciones médicas, Healthert inmediatamente te mandará una notificación a tu celular con el dato y la ubicación del individuo, con el objetivo de que la ayuda pueda acudir a tiempo y evitar situaciones desafortunadas.<br/>
-                    </p>
-
+                <div>
+                    {/*Div para texto que acompana*/}
                 </div>
+
             </div>
         </div>
     );
