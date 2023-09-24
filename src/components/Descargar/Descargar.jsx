27f9ba75a4ba8913861e1c2c9be521331207051c
Jairo Gonzalez <jairoirineo16@outlook.com>
2023-09-24 01:44:15 -0600

    Ultimas correcciones de tamano y posicion

--- src/components/Descargar/Descargar.jsx
+++ src/components/Descargar/Descargar.jsx
@@ -20,8 +20,8 @@ const Descargar = () => {
     return (
         
         <div className='flex justify-center'>
-            <div className='justify-center inline-block bg-slate-900 max-w-screen-xl'>
-            <div className='flex grid-cols-2 items-center content-center mt-8 ml-0 md:ml-20'>{/*flex grid-cols-2 gap-2  */}
+            <div className='justify-center inline-block dark:bg-slate-900 max-w-screen-xl'>
+            <div className='flex grid-cols-2 items-center content-center mt-8 ml-0 md:ml-10 lg:ml-28'>{/*flex grid-cols-2 gap-2  */}
                 <div className='flex justify-center ml-8  h-20 w-20 min-w-max min-h-max md:h-32 md:w-32 lg:h-40 lg:w-40 transform duration-700 '>
                     <img src={Logo} />
                 </div>
