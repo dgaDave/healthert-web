2abbc072a2f439d89ca31793109df42211dcf70e
GonAvi <dgaduff@gmail.com>
2023-09-26 14:25:35 -0600

    Se anadieron los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,16 @@
 // Nosotros
 // Footer
 
+import NavBar from './components/NavBar/NavBar'
+import Inicio from './components/Inicio/Inicio'
+import Footer from './components/Footer/Footer'
+
 function App () {
   return (
     <>
+    <NavBar/>
+    <Inicio/>
+    <Footer/>
     </>
     
   )
