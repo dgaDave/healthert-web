0650c95b86d0521f4046edb31ac49ee67a6068f7
GonAvi <dgaduff@gmail.com>
2023-09-26 20:47:35 -0600

    Se hicierom cambios en footer y navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -21,12 +21,12 @@ const NavBar = () => {
       <div className='md:static md:translate-x-0 md:bg-transparent md:shadow-none absolute text-xl h-fit bg-white dark:bg-slate-900 shadow-lg inset-0 top-20 -translate-x-full p-2 rounded-b-xl peer-checked/menu:-translate-x-0 transition-all duration-200'>
         <ul className='md:flex  lg:space-x-4 md:space-x-2 lg:text-2xl items-center'>
           {/* ids de los titulos aqui */}
-          <li className='md:inline-block' onClick={clickMenu}><a className='glow' href="#">Servicios</a></li>
-          <li className='md:inline-block' onClick={clickMenu}><a className='glow' href="#">Descargar</a></li>
-          <li className='md:inline-block' onClick={clickMenu}><a className='glow' href="#">Planes</a></li>
-          <li className='md:inline-block' onClick={clickMenu}><a className='glow' href="#">Funciones</a></li>
-          <li className='md:inline-block' onClick={clickMenu}><a className='glow' href="#">Nosotros</a></li>
-          <li className='md:inline-block' onClick={clickMenu}>
+          <li className='w-full border-b-[1px] dark:border-slate-700 md:border-0 md:inline-block' onClick={clickMenu}><a className='glow w-full inline-block' href="#">Servicios</a></li>
+          <li className='w-full border-b-[1px] dark:border-slate-700 md:border-0 md:inline-block' onClick={clickMenu}><a className='glow w-full inline-block' href="#">Descargar</a></li>
+          <li className='w-full border-b-[1px] dark:border-slate-700 md:border-0 md:inline-block' onClick={clickMenu}><a className='glow w-full inline-block' href="#">Planes</a></li>
+          <li className='w-full border-b-[1px] dark:border-slate-700 md:border-0 md:inline-block' onClick={clickMenu}><a className='glow w-full inline-block' href="#">Funciones</a></li>
+          <li className='w-full border-b-[1px] dark:border-slate-700 md:border-0 md:inline-block' onClick={clickMenu}><a className='glow w-full inline-block' href="#">Nosotros</a></li>
+          <li className='w-full border-b-[1px] dark:border-slate-700 md:border-0 md:inline-block' onClick={clickMenu}>
             <a href="#">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 lg:w-12 lg:h-12 glow hidden md:inline">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
