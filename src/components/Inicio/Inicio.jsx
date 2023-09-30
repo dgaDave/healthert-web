2c1338b6f94360d84e673381f318f9e901b4156c
Jairo Gonzalez <jairoirineo16@outlook.com>
2023-09-30 11:13:53 -0600

    Acomodo de imagen y divs

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import mckphone from '../../assets/images/mckPhone1.png'
 import mckSmart from '../../assets/images/mckSmart1.png'
+import mcks from '../../assets/images/mcks1.png'
 
 
 const Inicio = () => {
   return (
     <>
-      <div className='flex'>
-        <div className="relative mx-auto max-w-screen-xl px-6 py-8 sm:py-12 sm:px-8 lg:py-16 lg:px-8">
-          <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+      <div className='flex justify-center'>
+        <div className="flex justify-center my-20 max-w-screen-lg">
+          <div className="grid grid-cols-1 gap-12 justify-center items-center lg:grid-cols-2">
             <div className="lg:py-24">
-              <h2 className="text-3xl font-bold text-center text-verde sm:text-7xl">HEALTHERT</h2>
-              <p className="mt-4 text-center sm:text-2xl">
-                Aplicación móvil de monitoreo y alerta remota
-              </p>
+              <div className='text-5xl font-bold text-center text-verde md:text-7xl'>
+                <h2>HEALTHERT</h2>
+              </div>
+              <div className='text-center text-base md:text-xl'>
+                <p>Aplicación móvil de monitoreo y alerta remota</p>
+              </div>
             </div>
-            <div className='flex gap-0 justify-center items-center'>
-              <div className='flex justify-center content-center animate-floating'>
-                <img alt='Dispositivo' src={mckSmart} className='h-auto w-[30rem]'></img>  
+            <div className='flex  justify-center content-center  mx-8 p-0 float-left items-center'>
+              <div className='flex justify-center content-center items-center animate-floating'>
+                <img alt='Dispositivo' src={mcks} className='h-auto w-96'></img>                
               </div>
-              <div className='flex justify-center content-center animate-floating'>
-                <img alt='Dispositivo' src={mckphone} className='h-auto w-[30rem]'></img>  
-              </div>
+              {/*<div className='flex justify-center content-center animate-floating'>
+                <img alt='Dispositivo' src={mckphone} className='h-auto w-96'></img>
+              </div>*/}
             </div>
           </div>
         </div>
