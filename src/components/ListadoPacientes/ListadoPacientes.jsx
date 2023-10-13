2c958392168b6e33345c2bc72827c15da5a7133a
Jairo Gonzalez <jairoirineo16@outlook.com>
2023-10-12 23:08:39 -0600

    Adapacion de las dos partes funcionales mostrar pacientes y info paciente

--- src/components/ListadoPacientes/ListadoPacientes.jsx
+++ src/components/ListadoPacientes/ListadoPacientes.jsx
@@ -12,34 +12,8 @@ const MostrarDoctorYPacientes = ({ doctor, pacientes }) => {
     return (
         <>
 
-        {infoDiv == 1 ?
-        <div className="lg:w-1/4  h-screen flex flex-col rounded-r-lg overflow-hidden shadow-lg border-gray-100 p-2 dark:bg-gray-400">
-            <a href="#" className="border-2 border-verde dark:border-slate-700 rounded-2xl bg-white p-2.5 my-5 flex items-center transition hover:scale-105 dark:bg-gray-800">
-                <img src="user.png" alt="Enfermera" className="rounded-full w-10 h-10 mr-4" />
-                <div>
-                    <p className="font-bold text-xl">{doctor.nombre}</p>
-                    <p>{doctor.especialidad}</p>
-                </div>
-            </a>
-            {/* <hr></hr><br></br> */}
-            <div className="scroll-m-0 overflow-y-scroll">
-                {pacientes.map(paciente => (
-                    <button onClick ={() => handleClick(paciente.nombre)} className="border-2 cursor-pointer border-verde rounded-2xl bg-white p-2 mt-5 mx-4 flex items-center mr-2 duration-300 transform hover:scale-105 dark:border-slate-700 dark:bg-gray-800 shadow-lg">
-
-                        <img src="user.png" alt="Paciente" className="rounded-full w-10 h-10 mx-4" />
-                        <div>
-                            <p className="font-bold text-lg">{paciente.nombre}</p>
-                            <p>Edad: {paciente.edad} &nbsp;&nbsp; BPM: {paciente.bpm}</p>
-                            <p>CURP: {paciente.curp}</p>
-                        </div>
-                    </button>
-
-                ))}
-            </div>
-        </div>
-
-        :
-        <><div className="lg:w-1/4  h-screen flex flex-col rounded-r-lg overflow-hidden shadow-lg border-gray-100 p-2 dark:bg-gray-400">
+            {infoDiv == 1 ?
+                <div className="lg:w-1/4  h-screen flex flex-col rounded-r-lg overflow-hidden shadow-lg border-gray-100 p-2 dark:bg-gray-400">
                     <a href="#" className="border-2 border-verde dark:border-slate-700 rounded-2xl bg-white p-2.5 my-5 flex items-center transition hover:scale-105 dark:bg-gray-800">
                         <img src="user.png" alt="Enfermera" className="rounded-full w-10 h-10 mr-4" />
                         <div>
@@ -50,7 +24,33 @@ const MostrarDoctorYPacientes = ({ doctor, pacientes }) => {
                     {/* <hr></hr><br></br> */}
                     <div className="scroll-m-0 overflow-y-scroll">
                         {pacientes.map(paciente => (
-                            <button onClick={() => handleClick(paciente.nombre)} className="border-2 cursor-pointer border-verde rounded-2xl bg-white p-2 mt-5 mx-4 flex items-center mr-2 duration-300 transform hover:scale-105 dark:border-slate-700 dark:bg-gray-800 shadow-lg">
+                            <button key={paciente.id} onClick={() => handleClick(paciente.nombre)} className="border-2 cursor-pointer border-verde rounded-2xl bg-white p-2 mt-5 mx-4 flex items-center mr-2 duration-300 transform hover:scale-105 dark:border-slate-700 dark:bg-gray-800 shadow-lg">
+
+                                <img src="user.png" alt="Paciente" className="rounded-full w-10 h-10 mx-4" />
+                                <div>
+                                    <p className="font-bold text-lg">{paciente.nombre}</p>
+                                    <p>Edad: {paciente.edad} &nbsp;&nbsp; BPM: {paciente.bpm}</p>
+                                    <p>CURP: {paciente.curp}</p>
+                                </div>
+                            </button>
+
+                        ))}
+                    </div>
+                </div>
+
+                :
+                <><div className="lg:w-1/4  h-screen flex flex-col rounded-r-lg overflow-hidden shadow-lg border-gray-100 p-2 dark:bg-gray-400">
+                    <a href="#" className="border-2 border-verde dark:border-slate-700 rounded-2xl bg-white p-2.5 my-5 flex items-center transition hover:scale-105 dark:bg-gray-800">
+                        <img src="user.png" alt="Enfermera" className="rounded-full w-10 h-10 mr-4" />
+                        <div>
+                            <p className="font-bold text-xl">{doctor.nombre}</p>
+                            <p>{doctor.especialidad}</p>
+                        </div>
+                    </a>
+                    {/* <hr></hr><br></br> */}
+                    <div className="scroll-m-0 overflow-y-scroll">
+                        {pacientes.map(paciente => (
+                            <button key={paciente.id} onClick={() => handleClick(paciente.nombre)} className="border-2 cursor-pointer border-verde rounded-2xl bg-white p-2 mt-5 mx-4 flex items-center mr-2 duration-300 transform hover:scale-105 dark:border-slate-700 dark:bg-gray-800 shadow-lg">
 
                                 <img src="user.png" alt="Paciente" className="rounded-full w-10 h-10 mx-4" />
                                 <div>
@@ -66,11 +66,11 @@ const MostrarDoctorYPacientes = ({ doctor, pacientes }) => {
                     altura={'180'} grupS={'0+'} genero={'Masculino'} numSeg={'4815163078686709'} alergias={'Ibuprofeno,Polen'} padecimientos={'Diabetes, hipertension'} /></>
 
 
-                }
-                
-                </>
+            }
 
-                
+        </>
+
+
     )
 }
 
@@ -96,7 +96,7 @@ const ListadoPacientes = () => {
         { id: 13, nombre: 'pepe', edad: '50 años', bpm: '78', curp: 1234567890 },
     ];
 
-    
+
 
     return (
         <MostrarDoctorYPacientes doctor={doctor} pacientes={pacientes} />
@@ -118,71 +118,78 @@ const Contenedor = ({ uno, dos, tres }) => {
         </div>
     )
 }
-const InfoCompleta = ({ nombre, curp , edad, peso,altura, grupS, genero, numSeg , alergias, padecimientos}) => {
+const InfoCompleta = ({ nombre, curp, edad, peso, altura, grupS, genero, numSeg, alergias, padecimientos }) => {
     const padecimiento = padecimientos.split(',');
     const alergia = alergias.split(',');
     const [isSmall, setIsSmall] = useState(false);
 
     const toggleSize = () => {
         setIsSmall(!isSmall);
+
     };
 
     const [listDiv, setListDiv] = useState(1);
 
-    return (
+    return (<>
+        {isSmall == false ?
+            <div className='flex absolute bottom-0 right-0 w-3/4 h-2/5 shadow-xl dark:bg-slate-800 p-5 transition transform duration-300 visible'>
+                <div className='grid justify-start items-center px-2'>
+                    <div className='border-2 border-verde dark:bg-slate-800 dark:border-slate-700 rounded-2xl hover:scale-105 duration-300 transform shadow-md p-4 lg:p-2 xl:p-4'>
+                        <div className='flex justify-start mb-2 font-semibold'>
+                            {nombre}
+                        </div>
+                        <div className='flex justify-start'>
+                            {curp}
+                        </div>
+                    </div>
+                    <Contenedor uno={'NSS: ' + numSeg} />
+                </div>
 
-       <div className={`flex absolute bottom-0 right-0 w-3/4 h-2/5 shadow-xl dark:bg-slate-800 p-5 transition transform duration-300 ${isSmall ? 'collapse' : 'vivisble'}`}>
-       <div className='grid justify-start items-center px-2'>
-           <div className='items-center'>
-               <img src='' alt='fotoPaciente '></img>
-           </div>
-           <div className='border-2 border-verde dark:bg-slate-800 dark:border-slate-700 rounded-2xl hover:scale-105 duration-300 transform shadow-md p-4 lg:p-2 xl:p-4'>
-               <div className='flex justify-start mb-2 font-semibold'>
-                   {nombre}
-               </div>
-               <div className='flex justify-start'>
-                   {curp}
-               </div>
-           </div>
-       </div>
+                <div className='grid justify-start items-center px-2'>
+                    <Contenedor uno={altura + ' cm'} dos={edad + ' años'} tres={peso + ' kg'} />
+                    <Contenedor uno={'Genero ' + genero} dos={'Tipo ' + grupS} />
+                </div>
+                <div className='grid mx-4 border-2 border-verde dark:bg-slate-800 dark:border-slate-700 rounded-2xl hover:scale-105 duration-300 transform shadow-md p-4 lg:p-2 xl:p-4'>
+                    <div className='flex justify-start mb-2'>
+                        <p className='font-semibold'>Alergias:</p>
+                    </div>
+                    <div className='grid justify-start mb-2  p-1'>
+                        {
+                            alergia.map((alergia, index) =>
+                                <li key={index}>{alergia}</li>)
+                        }
+                    </div>
+                </div>
+                <div className='grid mx-4 border-2 border-verde dark:bg-slate-800 dark:border-slate-700 rounded-2xl hover:scale-105 duration-300 transform shadow-md p-4 lg:p-2 xl:p-4'>
+                    <div className='flex justify-start mb-2'>
+                        <p className='font-semibold'>Padecimientos:</p>
+                    </div>
+                    <div className='grid justify-start mb-2  p-1'>
+                        {
+                            padecimiento.map((padecimiento, index) =>
+                                <li key={index}>{padecimiento}</li>)
+                        }
+                    </div>
+                </div>
+                <div className='grid justify-center items-start px-2'>
+                    <a href='#' className='flex bg-verde px-2 py-1 rounded-xl hover:bg-red-600 duration-300 transform' onClick={toggleSize}>
+                        <svg xmlns='http://www.w3.org/2000/svg' className='h-8 w-8 fill-black dark:fill-white' viewBox='0 0 24 24'>
+                            <path d='M19 12a1 1 0 0 1-1 1H8.414l1.293 1.293a1 1 0 1 1-1.414 1.414l-3-3a1 1 0 0 1 0-1.414l3-3a1 1 0 0 1 1.414 1.414L8.414 11H18a1 1 0 0 1 1 1z' />
+                        </svg>
+                    </a>
+                </div>
 
-       <div className='grid justify-start items-center px-2'>
-           <Contenedor uno={'NSS: ' + numSeg} />
-           <Contenedor uno={altura + ' cm'} dos={edad + ' años'} tres={peso + ' kg'} />
-           <Contenedor uno={'Genero ' + genero} dos={'Tipo ' + grupS} />
-       </div>
-       <div className='grid mx-4 border-2 border-verde dark:bg-slate-800 dark:border-slate-700 rounded-2xl hover:scale-105 duration-300 transform shadow-md p-4 lg:p-2 xl:p-4'>
-           <div className='flex justify-start mb-2'>
-               <p className='font-semibold'>Alergias:</p>
-           </div>
-           <div className='grid justify-start mb-2  p-1'>
-               {
-                   alergia.map((alergia, index) =>
-                       <li key={index}>{alergia}</li>)
-               }
-           </div>
-       </div>
-       <div className='grid mx-4 border-2 border-verde dark:bg-slate-800 dark:border-slate-700 rounded-2xl hover:scale-105 duration-300 transform shadow-md p-4 lg:p-2 xl:p-4'>
-           <div className='flex justify-start mb-2'>
-               <p className='font-semibold'>Padecimientos:</p>
-           </div>
-           <div className='grid justify-start mb-2  p-1'>
-               {
-                   padecimiento.map((padecimiento, index) =>
-                       <li key={index}>{padecimiento}</li>)
-               }
-           </div>
-       </div>
-       <div className='grid justify-center items-start px-2'>
-           <a href='#' className='flex bg-verde px-2 py-1 rounded-xl hover:bg-red-600 duration-300 transform' onClick={toggleSize}>
-               <svg xmlns='http://www.w3.org/2000/svg' className='h-8 w-8 fill-black dark:fill-white' viewBox='0 0 24 24'>
-                   <path d='M19 12a1 1 0 0 1-1 1H8.414l1.293 1.293a1 1 0 1 1-1.414 1.414l-3-3a1 1 0 0 1 0-1.414l3-3a1 1 0 0 1 1.414 1.414L8.414 11H18a1 1 0 0 1 1 1z' />
-               </svg>
-           </a>
-       </div>
-
-   </div>
-
+            </div> :
+            <div className='flex absolute bottom-0 right-0 w-3/4 h-8 shadow-xl dark:bg-slate-800 p-5 transition transform duration-300 visible'>
+                <div className='flex justify-end items-end px-2'>
+                    <a href='#' className='flex bg-verde px-2 py-1 rounded-xl hover:bg-red-600 duration-300 transform' onClick={toggleSize}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className='h-8 w-8 fill-black dark:fill-white'  viewBox="0 0 24 24">
+                            <path d="M7.33199 7.68464C6.94146 8.07517 6.3083 8.07517 5.91777 7.68464C5.52725 7.29412 5.52725 6.66095 5.91777 6.27043L10.5834 1.60483C11.3644 0.823781 12.6308 0.82378 13.4118 1.60483L18.0802 6.27327C18.4707 6.66379 18.4707 7.29696 18.0802 7.68748C17.6897 8.078 17.0565 8.078 16.666 7.68748L13 4.02145V21.9999C13 22.5522 12.5523 22.9999 12 22.9999C11.4477 22.9999 11 22.5522 11 21.9999V4.01666L7.33199 7.68464Z" fill="#0F0F0F" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        }</>
     )
 }
 
