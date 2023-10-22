7191a41932cb38c7f2c52a9a30eba8f27fab5a48
GonAvi <dgaduff@gmail.com>
2023-10-22 09:52:35 -0600

    Se agrego la configuracion basica

--- src/components/GoogleMap/GoogleMap.jsx
+++ src/components/GoogleMap/GoogleMap.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
 const GoogleMap = () => {
+    const mapOptions = {
+        zoom: 18,
+        center: {
+            lat: 43.68,
+            lng: -79.43
+        },
+        minZoom: 15,
+        maxZoom: 22,
+        disableDefaultUI: true,
+        styles: styles
+    }
+
+    const [mapContainer, setMapContainer] = useState(null)
+
     return (
-        <div>GoogleMap</div>
+        <div className='h-screen w-screen flex flex-row-reverse'>
+            <GoogleMapsProvider
+                googleMapsAPIKey='AIzaSyBbY_7khNJQ706ykCfevkbBMEG3twrf_-Y'
+                mapOptions={mapOptions}
+                mapContainer={mapContainer} >
+                <div className='h-full w-[75%]' ref={(node) => setMapContainer(node)} />
+            </GoogleMapsProvider >
+        </div>
+
     )
 }
 
