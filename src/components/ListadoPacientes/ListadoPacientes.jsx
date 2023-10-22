3f3c25302436833e296ec58bc03338daa298d656
GonAvi <dgaduff@gmail.com>
2023-10-22 12:53:27 -0600

    Moved data to another file

--- src/components/ListadoPacientes/ListadoPacientes.jsx
+++ src/components/ListadoPacientes/ListadoPacientes.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import CardListado from './CardListado';
+import {doctor, pacientes} from './DummyData'
 
 const MostrarDoctorYPacientes = ({ doctor, pacientes }) => {
     const [nombrePaciente, setNombrePaciente] = useState(null);
@@ -27,25 +28,7 @@ const MostrarDoctorYPacientes = ({ doctor, pacientes }) => {
 
 const ListadoPacientes = () => {
     // Datos de ejemplo
-    const doctor = {
-        nombre: 'Lucía Pérez',
-        especialidad: 'Enfermera/o'
-    };
-
-    const pacientes = [
-        { id: 1, nombre: 'Paciente 1', edad: '30 años', bpm: '72', curp: 1234567890 },
-        { id: 2, nombre: 'Paciente 2', edad: '40 años', bpm: '75', curp: 1234567890 },
-        { id: 3, nombre: 'Paciente 3', edad: '50 años', bpm: '78', curp: 1234567890 },
-        { id: 5, nombre: 'Leal', edad: '50 años', bpm: '78', curp: 1234567890 },
-        { id: 6, nombre: 'Paciente 1', edad: '30 años', bpm: '72', curp: 1234567890 },
-        { id: 7, nombre: 'Paciente 2', edad: '40 años', bpm: '75', curp: 1234567890 },
-        { id: 8, nombre: 'Paciente 3', edad: '50 años', bpm: '78', curp: 1234567890 },
-        { id: 9, nombre: 'Leal', edad: '50 años', bpm: '78', curp: 1234567890 },
-        { id: 10, nombre: 'Paciente 1', edad: '30 años', bpm: '72', curp: 1234567890 },
-        { id: 11, nombre: 'Paciente 2', edad: '40 años', bpm: '75', curp: 1234567890 },
-        { id: 12, nombre: 'pepe 3', edad: '50 años', bpm: '78', curp: 1234567890 },
-        { id: 13, nombre: 'pepe', edad: '50 años', bpm: '78', curp: 1234567890 },
-    ];
+    
 
 
 
